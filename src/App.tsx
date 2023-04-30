ca952dcd25bd2608a985e5b536aa282286547a0e
Jackson <jackson@classum.com>
2023-04-30 20:37:44 +0900

    Init html2canvas package

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import html2canvas from 'html2canvas';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const onSaveAs = (uri: string, fileName: string) => {
+    const link = document.createElement('a');
+    document.body.appendChild(link);
+    link.href = uri;
+    link.download = fileName;
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const downloadImage = async () => {
+    const container = document.getElementById('card');
+    if (!container) {
+      alert('Unexpected error');
+      return;
+    }
+
+    const canvas = await html2canvas(container);
+    onSaveAs(canvas.toDataURL('image/png'), 'result.png');
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div id="card">
+        <button onClick={() => downloadImage()}>count is</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
